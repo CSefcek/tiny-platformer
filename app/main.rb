c8078b9839c080f452912352b2de5519a737f44d
CSefcek <carlosefcek@gmail.com>
2025-11-03 01:02:54 +0100

    Add basic player movement

--- app/main.rb
+++ app/main.rb
@@ -1,85 +1,65 @@
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
+  # Black background
+  args.outputs.solids << {
+    x: 0,
+    y: 0,
+    w: args.grid.w,
+    h: args.grid.h,
+    r: 0,
+    g: 0,
+    b: 0,
+  }
+  args.state.terrain ||= [
+      { x: 640,          y: 360,          w: 32, h: 32, path: 'sprites/square/blue.png' },
+      { x: 640,          y: 360 - 32,     w: 32, h: 32, path: 'sprites/square/blue.png' },
+      { x: 640,          y: 360 - 32 * 2, w: 32, h: 32, path: 'sprites/square/blue.png' },
+      { x: 640 + 32,     y: 360 - 32 * 2, w: 32, h: 32, path: 'sprites/square/blue.png' },
+      { x: 640 + 32 * 2, y: 360 - 32 * 2, w: 32, h: 32, path: 'sprites/square/blue.png' },
+    ]
+
+  args.state.player ||= { x: 576,
+                          y: 200,
+                          w: 80,
+                          h: 80,
+                          path: '/sprites/square/green.png' }
 
   args.outputs.labels  << { x: 640,
                             y: 600,
-                            text: 'Hello World!',
+                            text: 'Score: ',
                             size_px: 30,
                             anchor_x: 0.5,
                             anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "You can also access these docs online at docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  args.outputs.sprites << args.state.player
 
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  args.outputs.sprites << args.state.terrain
 
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
 
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
 
   if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
+    args.state.player.x -= 10
   elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+    args.state.player.x += 10
   end
 
   if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
+    args.state.player.y -= 10
   elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+    args.state.player.y += 10
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
+  if args.state.player.x > 1280
+    args.state.player.x = 0
+  elsif args.state.player.x < 0
+    args.state.player.x = 1280
   end
 
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
+  if args.state.player.y > 720
+    args.state.player.y = 0
+  elsif args.state.player.y < 0
+    args.state.player.y = 720
   end
 end
+
+$gtk.reset
